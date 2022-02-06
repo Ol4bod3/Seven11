ecc71e4bde5a6df1e3fdafbd970af14389f24867
Ol4bod3 <no.pl4c3.l1k3.127.0.0.1@gmail.com>
2022-02-06 03:14:51 +0100

    Mobile responsive

--- JS/index.js
+++ JS/index.js
@@ -1,10 +1,37 @@
+document.onreadystatechange = () => {
+  if (document.readyState != "complete") {
+    document.querySelector("body").style.visibility = "hidden";
+    document.querySelector("#loader").style.visibility = "visible";
+  } else {
+    document.querySelector("#loader").style.display = "none";
+    document.querySelector("body").style.visibility = "visible";
+  }
+};
+
+const hamBurger = document.querySelector(".hamBurger");
+const nav = document.querySelector(".nav");
+const navOpen = document.querySelector(".navOpen");
+const navLink = document.querySelectorAll("nav a");
+
+hamBurger.addEventListener("click", () => {
+  nav.classList.toggle("navOpen");
+  for (var i = 0; i < navLink.length; i++) {
+    navLink[i].addEventListener("click", () => {
+      nav.classList.remove("navOpen");
+    });
+  }
+});
+
+// navLink.addEventListener("click", () => {
+//   console.log(navLink.value);
+// });
+
 var prevPos = window.pageXOffset;
 window.onscroll = function () {
   var currentPos = window.pageYOffset;
   if (prevPos > currentPos) {
     document.querySelector(".navBar").style.top = "0";
     document.querySelector(".navBar").style.background = "#191921";
-    console.log(currentPos);
     if (currentPos == 0) {
       document.querySelector(".navBar").style.background = "transparent";
     }
